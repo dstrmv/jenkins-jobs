e01c92897d46d99a073ef36760363c9a37b39112
ivbu0719 <ivan.bulychev@netcracker.com>
2020-04-09 23:58:01 +0400

    test folders

--- hello.groovy
+++ hello.groovy
@@ -3,4 +3,6 @@
 node("master") {
   hello()
   echo sh(script: 'env|sort', returnStdout: true)
+  def folders = Jenkins.instance.getAllItems(com.cloudbees.hudson.plugins.folder.Folder.class)
+  echo "${folders[0].getName()}"
 }
