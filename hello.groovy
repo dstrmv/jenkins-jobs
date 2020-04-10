c973a98c0829d82745f5eb35c5450ed9dea59a2d
ivbu0719 <ivan.bulychev@netcracker.com>
2020-04-10 10:49:14 +0400

    get

--- hello.groovy
+++ hello.groovy
@@ -4,5 +4,9 @@ node("master") {
   hello()
   echo sh(script: 'env|sort', returnStdout: true)
   def folders = Jenkins.get().getAllItems(com.cloudbees.hudson.plugins.folder.Folder.class)
+  def jobs = Jenkins.get().getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob)
   echo "${folders[0].getName()}"
+  jobs.each {
+    echo "${it.getName()}"
+  }
 }
